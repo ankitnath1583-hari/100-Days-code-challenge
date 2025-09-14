b944f3258cbe1d598d27a1fabafe4b33e08db080
Ankitnath <ankitnath1583@gmail.com>
2025-09-14 22:35:19 +0530

    Add files via upload

---
+++ problem_46.c
@@ -0,0 +1,34 @@
+/*Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int n = 5;  
+
+    for (i = 0; i < n; i++) {       
+        for (j = 0; j < n; j++) {  
+            printf("*");
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
