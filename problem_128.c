8e4b2af624896c0431f34c205300d09a465bdc86
Ankitnath <ankitnath1583@gmail.com>
2025-11-25 10:38:28 +0530

    Add files via upload

--- problem_128.c
+++ problem_128.c
@@ -11,33 +11,24 @@ Consonants: 10
 */
 #include <stdio.h>
 #include <ctype.h>
-
 int main() {
     FILE *fp;
-    char ch;
+ char ch;
     int vowels = 0, consonants = 0;
-
     fp = fopen("text.txt", "r");
     if (fp == NULL) {
-        printf("Error: File not found!\n");
-        return 1;
-    }
-
+printf("Error: File not found!\n");
+return 1; }
     while ((ch = fgetc(fp)) != EOF) {
-        ch = tolower(ch);
-
-        if (ch >= 'a' && ch <= 'z') {  // alphabet check
-            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
-                vowels++;
-            else
-                consonants++;
-        }
-    }
-
-    fclose(fp);
-
-    printf("Vowels: %d\n", vowels);
-    printf("Consonants: %d\n", consonants);
+ ch = tolower(ch);
+if (ch >= 'a' && ch <= 'z') { 
+ if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+   vowels++;
+ else
+ consonants++;}}
+fclose(fp);
+printf("Vowels: %d\n", vowels);
+ printf("Consonants: %d\n", consonants);
 
     return 0;
 }
