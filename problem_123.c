b35455f6515abd7d94504a20bb8da831dc13103f
Ankitnath <ankitnath1583@gmail.com>
2025-11-25 10:37:08 +0530

    Add files via upload

--- problem_123.c
+++ problem_123.c
@@ -18,30 +18,20 @@ int main() {
     char ch;
     int characters = 0, words = 0, lines = 0;
     int inWord = 0;
-
     fp = fopen("sample.txt", "r");
     if (fp == NULL) {
         printf("Error: File not found!\n");
         return 1;
     }
-
     while ((ch = fgetc(fp)) != EOF) {
-        characters++;   // count characters
-
-        if (ch == '\n')     // count lines
-            lines++;
-
-        // count words
+        characters++;  
+if (ch == '\n')     
+    lines++;
         if (ch == ' ' || ch == '\n' || ch == '\t') {
-            inWord = 0;
-        } else if (inWord == 0) {
+            inWord = 0; } else if (inWord == 0) {
             inWord = 1;
-            words++;
-        }
-    }
-
+            words++;} }
     fclose(fp);
-
     printf("Characters: %d\n", characters);
     printf("Words: %d\n", words);
     printf("Lines: %d\n", lines);
