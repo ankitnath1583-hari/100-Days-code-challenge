7015da77f91e1331b90d239203a8746ec95723c5
Ankitnath <ankitnath1583@gmail.com>
2025-11-25 10:56:22 +0530

    Add files via upload

--- problem_123.c
+++ problem_123.c
@@ -11,30 +11,23 @@ Lines: 2
 
 */
 #include <stdio.h>
-#include <ctype.h>
-
 int main() {
     FILE *fp;
     char ch;
-    int characters = 0, words = 0, lines = 0;
-    int inWord = 0;
-    fp = fopen("sample.txt", "r");
-    if (fp == NULL) {
-        printf("Error: File not found!\n");
-        return 1;
-    }
-    while ((ch = fgetc(fp)) != EOF) {
-        characters++;  
-if (ch == '\n')     
-    lines++;
-        if (ch == ' ' || ch == '\n' || ch == '\t') {
-            inWord = 0; } else if (inWord == 0) {
-            inWord = 1;
-            words++;} }
-    fclose(fp);
-    printf("Characters: %d\n", characters);
-    printf("Words: %d\n", words);
-    printf("Lines: %d\n", lines);
+    int characters=0, words=0, lines=0, inWord=0;
 
+fp=fopen("sample.txt","r");
+    if(fp==NULL){
+ printf("file doesn't exist\n");
+return 1;}
+while((ch=fgetc(fp))!=EOF){
+        characters++;
+ if(ch=='\n') lines++;
+ if(ch==' '||ch=='\n'||ch=='\t') inWord=0;
+        else if(inWord==0){inWord=1;
+            words++; } }
+ if(characters>0) lines++;
+ fclose(fp);
+ printf("Characters: %d\nWords: %d\nLines: %d\n",characters,words,lines);
     return 0;
 }
